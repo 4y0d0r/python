dae2dad391f65564644e7d55157894c3dfeec694
4y0d0r <150987461+4y0d0r@users.noreply.github.com>
2024-10-13 07:10:08 -0500

    Add files via upload

---
+++ cookies.py
@@ -0,0 +1,13 @@
+# wanna see if i can write something by myself
+
+jar_of_cookies = 350.5
+
+chocolate_chip = jar_of_cookies*.3
+
+sugar = jar_of_cookies*.3
+
+cake = jar_of_cookies*.3
+
+print("This jar of cookies has 3 different types of cookies.\nChocolate Chip\nSugar\nCake")
+
+print("The total number of cookies in the jar is (jar_of_cookies)
